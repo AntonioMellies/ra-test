ab618bb3af8284f978645a5a8041dc812cf1491a
AntonioMellies <antoniomellies@gmail.com>
2020-01-22 20:09:52 -0300

    Domain module

---
+++ domain/src/main/java/models/Consumer.java
@@ -0,0 +1,16 @@
+package models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document("consumers")
+public class Consumer {
+
+
+
+}
