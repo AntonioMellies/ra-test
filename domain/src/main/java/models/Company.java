f66d1c2b430b62bb7be95993d6a8ac50caf0efe9
AntonioMellies <neto-fred@live.com>
2020-01-25 15:48:52 -0300

    Complaints informations endpoints

--- domain/src/main/java/models/Company.java
+++ domain/src/main/java/models/Company.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -21,7 +21,7 @@ public class Company {
     private String name;
 
     @GeoSpatialIndexed(type= GeoSpatialIndexType.GEO_2DSPHERE)
-    private GeoJsonPoint location;
+    private Point location;
 
     private String city;
 
